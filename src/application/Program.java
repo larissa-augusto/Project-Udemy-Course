ee2a0035c26c6352119ff2096e038a9aa6ab2d80
larissa-augusto <larissa.a.augusto@gmail.com>
2021-04-08 21:41:24 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,15 @@
 package application;
 
-import model.entities.Departament;
+import model.entities.Department;
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
 
 
-    Departament obj = new Departament(1, "Books");
-    System.out.println(obj);
+    Department obj = new Department(1, "Books");
+    Seller seller = new Seller(21, "Bob", "kjshdkjshdkjsh", new Date(), 3000.00, obj);
+    System.out.println(seller);
 }
 }
